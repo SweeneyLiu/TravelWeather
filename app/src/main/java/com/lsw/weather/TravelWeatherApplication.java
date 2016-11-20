3803ebb6f20398fa90b6551ae60e10d963010dc3
liushuwei <lsw305264677@163.com>
2016-11-20 19:28:49 +0800

    1.修改icon图标 2.增加侧边栏

---
+++ app/src/main/java/com/lsw/weather/TravelWeatherApplication.java
@@ -0,0 +1,10 @@
+package com.lsw.weather;
+
+import android.app.Application;
+
+public class TravelWeatherApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
