e4b6f6d3838e4f4f4512f91e57d5ddb33bb70da4
liushuwei <liushuwei@qiyi.com>
2017-01-18 19:01:09 +0800

    增加语音识别参数

--- app/src/main/java/com/lsw/weather/activity/MainActivity.java
+++ app/src/main/java/com/lsw/weather/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -31,8 +32,11 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
 import com.amap.api.location.AMapLocationListener;
+import com.baidu.tts.auth.AuthInfo;
+import com.baidu.tts.client.SpeechError;
 import com.baidu.tts.client.SpeechSynthesizer;
 import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
 import com.lsw.weather.R;
 import com.lsw.weather.adapter.DailyForecastAdapter;
 import com.lsw.weather.adapter.HourlyForecastAdapter;
@@ -46,6 +50,11 @@ import com.lsw.weather.util.SnackbarUtils;
 import com.lsw.weather.view.ScrollListView;
 import com.yanzhenjie.permission.AndPermission;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 
 import butterknife.BindView;
@@ -57,7 +66,7 @@ import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SpeechSynthesizerListener {
 
     @BindView(R.id.iv_weather_image)
     ImageView ivWeatherImage;
@@ -111,6 +120,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private static final String API_KEY = "LhS9kXyayGYZwrMY4lQ1Sh2F";
     private static final String SECRET_KEY = "db5b9a8f0403a921d1226673dffd0113";
 
+
+    private String mSampleDirPath;
+    private static final String SAMPLE_DIR_NAME = "baiduTTS";
+    private static final String SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female.dat";
+    private static final String SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male.dat";
+    private static final String TEXT_MODEL_NAME = "bd_etts_text.dat";
+    private static final String LICENSE_FILE_NAME = "temp_license";
+    private static final String ENGLISH_SPEECH_FEMALE_MODEL_NAME = "bd_etts_speech_female_en.dat";
+    private static final String ENGLISH_SPEECH_MALE_MODEL_NAME = "bd_etts_speech_male_en.dat";
+    private static final String ENGLISH_TEXT_MODEL_NAME = "bd_etts_text_en.dat";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -127,6 +148,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         Manifest.permission.READ_EXTERNAL_STORAGE,
                         Manifest.permission.READ_PHONE_STATE)
                 .send();
+
+//        initialEnv();
+//        initialTts();
+        voiceAnimation(fabSpeech,false);
 
         onLocationCity();
         swipeRefreshLayout.setRefreshing(true);
@@ -146,7 +171,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 if (mHeWeatherBean != null) {
                     mSpeechSynthesizer = SpeechSynthesizer.getInstance();
                     mSpeechSynthesizer.setContext(MainActivity.this);
-                    mSpeechSynthesizer.setAppId(APP_ID);
+//                    mSpeechSynthesizer.setAppId(APP_ID);
                     mSpeechSynthesizer.setApiKey(API_KEY, SECRET_KEY);
                     mSpeechSynthesizer.setAudioStreamType(AudioManager.STREAM_MUSIC);
                     String text = voiceWeather(MainActivity.this, mHeWeatherBean);
@@ -382,6 +407,111 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+
+    private void initialEnv() {
+        if (mSampleDirPath == null) {
+            String sdcardPath = Environment.getExternalStorageDirectory().toString();
+            mSampleDirPath = sdcardPath + "/" + SAMPLE_DIR_NAME;
+        }
+        makeDir(mSampleDirPath);
+        copyFromAssetsToSdcard(false, SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/" + SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, TEXT_MODEL_NAME, mSampleDirPath + "/" + TEXT_MODEL_NAME);
+        copyFromAssetsToSdcard(false, LICENSE_FILE_NAME, mSampleDirPath + "/" + LICENSE_FILE_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_SPEECH_MALE_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_SPEECH_MALE_MODEL_NAME);
+        copyFromAssetsToSdcard(false, "english/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath + "/"
+                + ENGLISH_TEXT_MODEL_NAME);
+    }
+
+    private void makeDir(String dirPath) {
+        File file = new File(dirPath);
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+    }
+
+    /**
+     * 将sample工程需要的资源文件拷贝到SD卡中使用（授权文件为临时授权文件，请注册正式授权）
+     *
+     * @param isCover 是否覆盖已存在的目标文件
+     * @param source
+     * @param dest
+     */
+    private void copyFromAssetsToSdcard(boolean isCover, String source, String dest) {
+        File file = new File(dest);
+        if (isCover || (!isCover && !file.exists())) {
+            InputStream is = null;
+            FileOutputStream fos = null;
+            try {
+                is = getResources().getAssets().open(source);
+                String path = dest;
+                fos = new FileOutputStream(path);
+                byte[] buffer = new byte[1024];
+                int size = 0;
+                while ((size = is.read(buffer, 0, 1024)) >= 0) {
+                    fos.write(buffer, 0, size);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    if (is != null) {
+                        is.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void initialTts() {
+        this.mSpeechSynthesizer = SpeechSynthesizer.getInstance();
+        this.mSpeechSynthesizer.setContext(this);
+        this.mSpeechSynthesizer.setSpeechSynthesizerListener(this);
+        // 文本模型文件路径 (离线引擎使用)
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_TEXT_MODEL_FILE, mSampleDirPath + "/"
+                + TEXT_MODEL_NAME);
+        // 声学模型文件路径 (离线引擎使用)
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, mSampleDirPath + "/"
+                + SPEECH_FEMALE_MODEL_NAME);
+        // 本地授权文件路径,如未设置将使用默认路径.设置临时授权文件路径，LICENCE_FILE_NAME请替换成临时授权文件的实际路径，仅在使用临时license文件时需要进行设置，如果在[应用管理]中开通了正式离线授权，不需要设置该参数，建议将该行代码删除（离线引擎）
+        // 如果合成结果出现临时授权文件将要到期的提示，说明使用了临时授权文件，请删除临时授权即可。
+//        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_TTS_LICENCE_FILE, mSampleDirPath + "/" + LICENSE_FILE_NAME);
+        // 请替换为语音开发者平台上注册应用得到的App ID (离线授权)
+        this.mSpeechSynthesizer.setAppId("9204443");/*这里只是为了让Demo运行使用的APPID,请替换成自己的id。*/
+        // 请替换为语音开发者平台注册应用得到的apikey和secretkey (在线授权)
+        this.mSpeechSynthesizer.setApiKey("LhS9kXyayGYZwrMY4lQ1Sh2F", "db5b9a8f0403a921d1226673dffd0113");/*这里只是为了让Demo正常运行使用APIKey,请替换成自己的APIKey*/
+        // 发音人（在线引擎），可用参数为0,1,2,3。。。（服务器端会动态增加，各值含义参考文档，以文档说明为准。0--普通女声，1--普通男声，2--特别男声，3--情感男声。。。）
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
+        // 设置Mix模式的合成策略
+        this.mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
+        // 授权检测接口(只是通过AuthInfo进行检验授权是否成功。)
+        // AuthInfo接口用于测试开发者是否成功申请了在线或者离线授权，如果测试授权成功了，可以删除AuthInfo部分的代码（该接口首次验证时比较耗时），不会影响正常使用（合成使用时SDK内部会自动验证授权）
+        AuthInfo authInfo = this.mSpeechSynthesizer.auth(TtsMode.MIX);
+
+
+        // 初始化tts
+        mSpeechSynthesizer.initTts(TtsMode.MIX);
+        // 加载离线英文资源（提供离线英文合成功能）
+        /*int result = mSpeechSynthesizer.loadEnglishModel(mSampleDirPath + "/" + ENGLISH_TEXT_MODEL_NAME, mSampleDirPath
+                        + "/" + ENGLISH_SPEECH_FEMALE_MODEL_NAME);
+        Log.i("initialTts---","loadEnglishModel result=" + result);*/
+
+    }
+
     /**
      * 语音动画
      * @param fab
@@ -435,4 +565,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return sb.toString();
     }
 
+    @Override
+    public void onSynthesizeStart(String s) {
+
+    }
+
+    @Override
+    public void onSynthesizeDataArrived(String s, byte[] bytes, int i) {
+
+    }
+
+    @Override
+    public void onSynthesizeFinish(String s) {
+
+    }
+
+    @Override
+    public void onSpeechStart(String s) {
+
+    }
+
+    @Override
+    public void onSpeechProgressChanged(String s, int i) {
+
+    }
+
+    @Override
+    public void onSpeechFinish(String s) {
+
+    }
+
+    @Override
+    public void onError(String s, SpeechError error) {
+
+    }
 }
