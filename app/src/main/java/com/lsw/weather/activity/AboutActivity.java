d2105cc2865bae0767897193863ebcad61a38770
liushuwei <lsw305264677@163.com>
2017-02-08 00:14:28 +0800

    增加关于界面

--- app/src/main/java/com/lsw/weather/activity/AboutActivity.java
+++ app/src/main/java/com/lsw/weather/activity/AboutActivity.java
@@ -1,18 +1,35 @@
 package com.lsw.weather.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lsw.weather.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AboutActivity extends BaseActivity {
 
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.suggestion)
+    TextView suggestion;
+    @BindView(R.id.new_version)
+    TextView newVersion;
+    @BindView(R.id.favourable_comment)
+    TextView favourableComment;
+    @BindView(R.id.version)
+    TextView version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,56 @@ public class AboutActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.about);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @OnClick({R.id.suggestion, R.id.new_version, R.id.favourable_comment})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.suggestion:
+                break;
+            case R.id.new_version:
+                break;
+            case R.id.favourable_comment:
+                openApplicationMarket();
+                break;
+        }
+    }
+
+
+    /**
+     * 通过包名 在应用商店打开应用
+     */
+    private void openApplicationMarket() {
+        try {
+            String str = "market://details?id=" + getPackageName();
+            Intent localIntent = new Intent(Intent.ACTION_VIEW);
+            localIntent.setData(Uri.parse(str));
+            startActivity(localIntent);
+        } catch (Exception e) {
+            // 打开应用商店失败 可能是没有手机没有安装应用市场
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "打开应用商店失败", Toast.LENGTH_SHORT).show();
+            // 调用系统浏览器进入商城
+            String url = "http://app.mi.com/detail/163525?ref=search";
+            openLinkBySystem(url);
+        }
+    }
+
+    /**
+     * 调用系统浏览器打开网页
+     *
+     * @param url 地址
+     */
+    private void openLinkBySystem(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
 }
