bf9f48471e015c82fe4363ebefee71e6847e5a1f
liushuwei <lsw305264677@163.com>
2017-03-02 22:53:36 +0800

    获取全局的context

--- app/src/main/java/com/lsw/weather/TravelWeatherApplication.java
+++ app/src/main/java/com/lsw/weather/TravelWeatherApplication.java
@@ -1,18 +1,27 @@
 package com.lsw.weather;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.antfortune.freeline.FreelineCore;
 import com.squareup.leakcanary.LeakCanary;
 
 public class TravelWeatherApplication extends Application {
+
+    private static Context context;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        context = getApplicationContext();
         if (LeakCanary.isInAnalyzerProcess(this)) {
             return;
         }
         LeakCanary.install(this);
         FreelineCore.init(this);
+    }
+
+    public static Context getContext(){
+        return context;
     }
 }
