1239a228aa14c950cc594b973905156c895d4347
liushuwei <liushuwei@qiyi.com>
2017-03-02 11:08:02 +0800

    优化freeline

--- app/src/main/java/com/lsw/weather/TravelWeatherApplication.java
+++ app/src/main/java/com/lsw/weather/TravelWeatherApplication.java
@@ -2,9 +2,17 @@ package com.lsw.weather;
 
 import android.app.Application;
 
+import com.antfortune.freeline.FreelineCore;
+import com.squareup.leakcanary.LeakCanary;
+
 public class TravelWeatherApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        LeakCanary.install(this);
+        FreelineCore.init(this);
     }
 }
